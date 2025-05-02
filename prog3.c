4a4917293ebea39ade09de6685e8cd82f42ebc49
EshwarTeja-17 <eshwarvalaboju17@gmail.com>
2025-05-02 16:25:13 +0530

    odd or even

---
+++ prog3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter a number : ");
+	scanf("%d",&num);
+	if(num%2)
+	{
+		printf("its an odd number\n");
+	}
+	else
+	{
+		printf("its an even number\n");
+	}
+}
